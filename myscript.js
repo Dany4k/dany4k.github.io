efa3afab5a62023a77b8419ec292f99cb04d7233
Dany4k <dany4k@gmail.com>
2023-03-16 13:16:10 +0000

    Added Redirects

---
+++ myscript.js
@@ -0,0 +1,11 @@
+function myFunction1() {
+    window.open("https://www.idtraining.pt/");
+}
+
+function myFunction2() {
+    window.open("https://www.infraestruturasdeportugal.pt/");
+}
+
+function myFunction3() {
+    window.open("https://blueeconomybarometer.pwc.pt/");
+}
