119ff4778a451feb8506f8cdde9d02a6866d7336
Dany4k <dany4k@gmail.com>
2024-08-19 14:54:39 +0100

    JS Fix

--- myscript.js
+++ myscript.js
@@ -9,16 +9,3 @@ function myFunction2() {
 function myFunction3() {
   window.open("https://blueeconomybarometer.pwc.pt/");
 }
-
-window.onscroll = function () {
-  myFunction();
-};
-
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height =
-    document.documentElement.scrollHeight -
-    document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
