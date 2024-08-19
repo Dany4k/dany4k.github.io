de4c1ec36930245677371d7c31017fa799cb9414
Dany4k <dany4k@gmail.com>
2024-08-19 14:52:42 +0100

    JS Fix

--- myscript.js
+++ myscript.js
@@ -1,11 +1,23 @@
 function myFunction1() {
-    window.open("https://www.idtraining.pt/");
+  window.open("https://www.idtraining.pt/");
 }
 
 function myFunction2() {
-    window.open("https://www.infraestruturasdeportugal.pt/");
+  window.open("https://www.infraestruturasdeportugal.pt/");
 }
 
 function myFunction3() {
-    window.open("https://blueeconomybarometer.pwc.pt/");
+  window.open("https://blueeconomybarometer.pwc.pt/");
+}
+
+window.onscroll = function () {
+  myFunction();
+};
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
 }
